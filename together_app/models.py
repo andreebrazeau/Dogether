5327b1e8600f844bf7a20d929f978c7de9dabfe6
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-25 15:12:45 -0700

    added base.html

--- together_app/models.py
+++ together_app/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
 	user_id = models.CharField(max_length=200)
 	title = models.CharField(max_length=200)
 	create_at = models.DateTimeField(auto_now_add=True)
-	details = models.TextField(blank=False)
+	details = models.TextField( null=True, blank=True)
 
 	def __unicode__(self):
 		return self.title
@@ -13,10 +13,10 @@ class Project(models.Model):
 class Job(models.Model):
 	title = models.CharField(max_length=200)
 	created_at = models.DateTimeField(auto_now_add=True)
-	note = models.TextField(blank=False)
-	assign_to = models.CharField(max_length=200,blank=False)  #models.ForeignKey(User) for later
-	due_date = models.DateField(blank=False)
-	parent = models.ForeignKey('self', blank=False)
+	note = models.TextField(blank=True, null=True)
+	assign_to = models.CharField(max_length=200, null=True, blank=True)  #models.ForeignKey(User) for later
+	due_date = models.DateField(blank=True, null = True)
+	parent = models.ForeignKey('self', blank=True, null=True)
 	project_id = models.ForeignKey(Project)
 	completed = models.BooleanField(default = False)
 	
--- together_app/views.py
+++ together_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from models import Project, Job
 
 def home(request):
-    jobs = Job.objects.all()
-    project = Project.objects.get(id = jobs[0].project_id)
+    project = Project.objects.get(id = 1)
+    jobs = Job.objects.filter(project_id = project.id) 
     print jobs
-    return render_to_response('home.html', {'jobs' : jobs})
+    return render_to_response('home.html', {'jobs' : jobs, 'project' : project})
