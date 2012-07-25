06d0975277afe3c235d8dbf4a4d5660ca8fc326b
lkelly <llkelly88@gmail.com>
2012-07-24 18:04:48 -0700

    added project title

--- together_app/views.py
+++ together_app/views.py
@@ -3,5 +3,6 @@ from models import Project, Job
 
 def home(request):
     jobs = Job.objects.all()
+    project = Project.objects.get(id = jobs[0].project_id)
     print jobs
     return render_to_response('home.html', {'jobs' : jobs})
