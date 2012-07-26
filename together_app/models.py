6f9d6426aaa06908a85a0c1976e3e2b29c5085d7
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-26 14:16:43 -0700

    form added

--- together_app/models.py
+++ together_app/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
 	user_id = models.CharField(max_length=200)
 	title = models.CharField(max_length=200)
 	create_at = models.DateTimeField(auto_now_add=True)
-	details = models.TextField( null=True, blank=True)
+	details = models.TextField(blank=True)
 
 	def __unicode__(self):
 		return self.title
@@ -13,12 +13,13 @@ class Project(models.Model):
 class Job(models.Model):
 	title = models.CharField(max_length=200)
 	created_at = models.DateTimeField(auto_now_add=True)
-	note = models.TextField(blank=True, null=True)
-	assign_to = models.CharField(max_length=200, null=True, blank=True)  #models.ForeignKey(User) for later
-	due_date = models.DateField(blank=True, null = True)
-	parent = models.ForeignKey('self', blank=True, null=True)
+	note = models.TextField(blank=True)
+	assign_to = models.CharField(max_length=200, blank=True)  #models.ForeignKey(User, null = True) for later
+	due_date = models.DateField(null = True, blank = True)
+	parent = models.ForeignKey('self', null=True)
 	project_id = models.ForeignKey(Project)
 	completed = models.BooleanField(default = False)
+	order = models.IntegerField(null=True)
 	
 	def __unicode__(self):
 		return self.title
--- together_app/views.py
+++ together_app/views.py
@@ -3,6 +3,6 @@ from models import Project, Job
 
 def home(request):
     project = Project.objects.get(id = 1)
-    jobs = Job.objects.filter(project_id = project.id) 
+    jobs = Job.objects.filter(project_id = project.id).order_by('order')
     print jobs
-    return render_to_response('home.html', {'jobs' : jobs, 'project' : project})
+    return render_to_response('tasks.html', {'jobs' : jobs, 'project' : project})
