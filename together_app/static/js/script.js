08fbad5a6d205e63dc76082077002341790171d7
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-26 17:58:23 -0700

    post work and show task on screen

--- together_app/static/js/script.js
+++ together_app/static/js/script.js
@@ -9,14 +9,17 @@ function add_job(){
     var note = $('#note').val();
     var due_date = $('#due_date').val();
     var assign_to = $('#assign_to').val();
-    console.log(title);
+
     $.ajax({
         type: "POST",
         url: "/add_job",
-        data: { title: title, note: note, due_date:due_date, assign_to:assign_to}
-    }).done(function( msg ) {
-        alert( title);
-    });
+        data: { title: title, note: note, due_date:due_date, assign_to:assign_to, project_id:project_id}
+    }).done(add_job_to_page);
+    return false;
+};
 
-
+function add_job_to_page(data) {
+    console.log(data.title)
+    var new_job = '<tr><td>?</td><td><input type="checkbox"></td><td>'+ data.title +'</td><td class = "right-row">></td></tr>';
+    $('#job_table').append(new_job)
 };
--- together_app/views.py
+++ together_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
 from models import Project, Job
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+# from json_data import *
+from django.http import HttpResponse
+import json
 
 def home(request, project_id):
     project = Project.objects.get(id = project_id)
@@ -15,16 +18,19 @@ def add_job(request):
 	note = request.POST.get('note')
 	due_date = request.POST.get('due_date')
 	assign_to = request.POST.get('assign_to')
+	project_id = request.POST.get('project_id')
+	project = Project.objects.get(id = project_id)
 	print 'This:', title, note, due_date, assign_to
-	# new_job = Job(
-	# 	title = title,
-	#     note = note,
-	#     due_date = due_date,
-	#     assign_to = assign_to,
-	#     #parent = 
-	#     project_id = project_id
-	#     #order = 
-	# )
-	
-	# new_job.save()
-	return render_to_response('tasks.html')
+	new_job = Job(
+		title = title,
+	    note = note,
+	    due_date = due_date,
+	    assign_to = assign_to,
+	    #parent = 
+	    project_id = project
+	    #order = 
+	)
+	new_job.save()
+	result = {'title' : title}
+	data = json.dumps(result)
+	return HttpResponse(data, "application/json")
