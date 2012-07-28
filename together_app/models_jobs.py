49fe244fc2bbcafbd16db073667436d1afb47d82
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-28 15:38:31 -0700

    seperate models

---
+++ together_app/models_jobs.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Project(models.Model):
+	user_id = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
+	create_at = models.DateTimeField(auto_now_add=True)
+	details = models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.title
+
